1045d5366672480e21252f72b372094082bde578
marinalls <marinalizaola16@gmail.com>
2025-06-08 22:29:13 -0600

    Cuarto commit

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,55 +1,70 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:google_fonts/google_fonts.dart';
 
 final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
-  // Do not add a build method, add a createState method
-  @override 
-  State<DiceRoller> createState() {
-    return _DiceRollerState();
-  }
 
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
 }
 
-// private class
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-2.png';
   var currentDiceRoll = 2;
 
   void rollDice() {
     setState(() {
-      currentDiceRoll = randomizer.nextInt(6) + 1; // Value between 1 and 6
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
-    // print('Changing image...');
   }
-  
+
   @override
   Widget build(context) {
     return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black45,
+                blurRadius: 15,
+                offset: Offset(0, 8),
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset(
               'assets/images/dice-$currentDiceRoll.png',
               width: 200,
             ),
-            const SizedBox(height: 20),
-            TextButton(
-                // onPressed: () {}),
-                onPressed: rollDice,
-                style: TextButton.styleFrom(
-                  // padding: const EdgeInsets.only(top: 20,),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                    fontSize: 28,
-                  ),
-                ),
-                child: const Text(
-                  'Roll Dice',
-                  style: TextStyle(color: Colors.white),
-                ))
-          ],
-        );
+          ),
+        ),
+        const SizedBox(height: 30),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            backgroundColor: const Color(0xFF1E3C72),
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+            textStyle: GoogleFonts.robotoMono(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.2,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            shadowColor: Colors.black54,
+            elevation: 6,
+          ),
+          child: const Text('Roll the Dice'),
+        ),
+      ],
+    );
   }
 }
